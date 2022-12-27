144d4e4bd1e865088b0d63944ffe34b13dca48d4
slaymed <mohabedr34@gmail.com>
2022-12-27 22:28:24 +0100

    Fixed Login logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -60,7 +60,6 @@ userRouter.post(
                     httpOnly: true,
                     secure: process.env.NODE_ENV === "production",
                     maxAge: 60 * 60 * 24 * 30,
-                    path: "/",
                 })
             );
 
@@ -192,7 +191,6 @@ userRouter.post(
                     httpOnly: true,
                     secure: process.env.NODE_ENV === "production",
                     maxAge: 60 * 60 * 24 * 30,
-                    path: "/",
                 })
             );
 
