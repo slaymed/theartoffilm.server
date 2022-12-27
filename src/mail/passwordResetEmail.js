e0a3099355f9eb6ed3c380d82fd66e3212d561cf
slaymed <mohabedr34@gmail.com>
2022-12-27 23:25:19 +0100

    Fixed reset password

--- src/mail/passwordResetEmail.js
+++ src/mail/passwordResetEmail.js
@@ -5,7 +5,7 @@ export const passwordResetEmail = ({ email, token }) => {
         subject: "Password Reset",
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
   Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
-  ${host}/reset/${token}\n\n
+  ${process.env.WEB_APP}/reset/${token}\n\n
   If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 };
